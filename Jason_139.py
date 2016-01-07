c4c72cd11f0089d7d509703791ed11c2579a0c88
Jason Broach <brojas7475@ccsdschools.com>
2016-01-07 13:10:34 -0500

    We put in our code in the canopy doc
    We put in our code and our strategy

--- Jason_139.py
+++ Jason_139.py
@@ -113,7 +113,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
         if getting_team_name:
             return 'loyal'
         else:
-            return 'c'
+            return 'b'
 
     
         
@@ -129,7 +129,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
         if getting_team_name:
             return 'backstabber'
         else:
-            return 'b'
+            return 'c'
 
 
 
@@ -226,16 +226,20 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'JB and ER'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            if score ==0: 
+                return 'b'
+            if opponent_score > score: 
+                return 'b'
             else:
-                return 'c' #otherwise collude
+                return 'c'
+                if opponent_history[-1] == 'c':
+                    return 'b'
+                else:
+                    return 'b'
+                    
+                    
     
     
     
